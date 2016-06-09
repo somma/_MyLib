5e72e74bdee8ade837c1181db5bd5118b1171dc8
somma <fixbrain@gmail.com>
2016-06-09 13:58:44 +0900

    a

--- wmi_client.cpp
+++ wmi_client.cpp
@@ -151,48 +151,44 @@ bool WmiClient::query(_In_ const wchar_t* query, _Out_ IEnumWbemClassObject*& en
     return true;
 }
 
-
 /// @brief 
 std::string variant_to_str(const VARIANT& var)
 {
-    if (var.vt == VT_NULL || var.vt == VT_EMPTY)
+    char buf[128] = { 0 };
+    switch (var.vt)
     {
+    case VT_NULL:
+    case VT_EMPTY:
         return "NULL";
-    }
-    else if (var.vt == VT_BOOL)
-    {
-        return var.boolVal ? "true" : "false";
-    }
-    else if (var.vt == VT_UI1)
-    {
-        char buf[128] = { 0, };
-        StringCbPrintfA(buf, sizeof(buf), "%c", var.cVal);
+    case VT_I2:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (int16_t)var.iVal);
         return std::string(buf);
-    }
-    else if (var.vt == VT_I2)
-    {
-        char buf[128] = { 0, };
-        StringCbPrintfA(buf, sizeof(buf), "%d", var.iVal);
+    case VT_I4:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (int32_t)var.lVal);
         return std::string(buf);
-    }
-    else if (var.vt == VT_I4)
-    {
-        char buf[128] = { 0, };
-        StringCbPrintfA(buf, sizeof(buf), "%d", var.lVal);
+    case VT_UI1:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (uint8_t)var.cVal);
         return std::string(buf);
-    }
-    else if (var.vt == VT_I8)
-    {
-        char buf[128] = { 0, };
-        StringCbPrintfA(buf, sizeof(buf), "%I64d", var.llVal);
+    case VT_UI2:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (uint16_t)var.uiVal);
         return std::string(buf);
-    }
-    else if (var.vt == VT_BSTR)
-    {
+    case VT_UI4:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (uint32_t)var.ulVal);
+        return std::string(buf);
+    case VT_INT:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (int32_t)var.intVal);
+        return std::string(buf);
+    case VT_UINT:
+        StringCbPrintfA(buf, sizeof(buf), "0x%x", (uint32_t)var.uintVal);
+        return std::string(buf);
+    case VT_BSTR:
         return std::string(WcsToMbsEx(var.bstrVal));
-    }
-
+    default:
+        StringCbPrintfA(buf, sizeof(buf), "unknwon type (vt=0x%x)", var.vt);
+        return std::string(buf);
+    };
 #pragma todo("complete this function...")
 
-    return "Unknown";
+    return "";
 }
+
