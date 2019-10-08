591c67143709c74f8c7f9efc6c44a681a5064e72
박제현 <48233976+jaehyeon406@users.noreply.github.com>
2019-10-08 12:03:14 +0900

    task_sched_client 리팩토링 (#52)
    _svc 멤버변수 이외의 변수들을 함수 내부에서 사용하고 자원을 해제하도록 수정

--- src/sched_client.h
+++ src/sched_client.h
@@ -14,6 +14,16 @@
 
 #pragma comment(lib,"taskschd.lib")
 
+#define _default_sched_folder_name L"\\"
+#define _default_day_interval 1
+
+#define _default_task_create_flag TASK_CREATE_OR_UPDATE | TASK_DISABLE
+#define _default_task_logon_type TASK_LOGON_INTERACTIVE_TOKEN
+
+#define _default_task_interval L"PT01M"
+#define _default_task_duration L"PT24H"
+#define _system_level_task_logon_user L"SYSTEM"
+
 typedef class SchedClient
 {
 public:
@@ -23,46 +33,50 @@ public:
 	bool initialize();
 	void finalize();
 	
-	bool create(_In_ const wchar_t* task_name);
-	bool easy_create(_In_ const wchar_t* task_name,
-					 _In_ const wchar_t* execute_path,
-					 _In_ const wchar_t* arguments,
-					 _In_ uint32_t interval);
+	bool initialized();
 
-	bool set_author(_In_ const wchar_t* author);
-	
-	bool set_trigger_daily(_In_ const wchar_t* start_time,
-						   _In_ const wchar_t* end_time,
-						   _In_ short days_interval,
-						   _In_ bool enabled);
+	bool create_daily_task(_In_ const wchar_t* task_name,
+						   _In_ const wchar_t* execute_path,
+						   _In_ const wchar_t* arguments,
+						   _In_opt_ const wchar_t* folder_name = _default_sched_folder_name,
+						   _In_opt_ short day_interval = _default_day_interval,
+						   _In_opt_ const wchar_t* task_interval = _default_task_interval,
+						   _In_opt_ const wchar_t* task_duration = _default_task_duration);
 
-	bool set_daily_repet(_In_ const wchar_t* interval = L"PT1H",
-						 _In_ const wchar_t* duration = L"PT24H");
+	bool remove_task(_In_ const wchar_t* task_name,
+					 _In_opt_ const wchar_t* folder_name = _default_sched_folder_name);
 
-	bool set_execute(_In_ const wchar_t* execute_path,
-					 _In_ const wchar_t* arguments);
-	
-	bool save(_In_ TASK_CREATION flag = TASK_CREATE_OR_UPDATE);
-	bool remove(_In_ const wchar_t* task_name);
-	
-	bool enabled(_In_ const wchar_t* task_name);
-	bool disabled(_In_ const wchar_t* task_name);
+	bool change_task_status(_In_ const wchar_t* task_name,
+							_In_opt_ bool enabled_task = true,
+							_In_opt_ const wchar_t* folder_name = _default_sched_folder_name);
 
-	bool is_task_existW(_In_ const wchar_t* task_name);
-	void clear();
+	bool is_task_existW(_In_ const wchar_t* task_name,
+						_In_opt_ const wchar_t* folder_name = _default_sched_folder_name);
+
+	bool get_task_action_exec_programs(_In_ const wchar_t* task_name,
+									   _Out_ std::vector<std::wstring>& task_exec_path,
+									   _In_opt_ const wchar_t* folder_name = _default_sched_folder_name);
+
+
+private:
+	bool make_daily_trigger(_In_ ITriggerCollection* trigger_collection,
+							_In_opt_ short day_interval = _default_day_interval,
+							_In_opt_ const wchar_t* task_interval = _default_task_interval,
+							_In_opt_ const wchar_t* task_duration = _default_task_duration);
+
+	bool make_actions(_In_ ITaskDefinition* definition,
+					  _In_ const wchar_t* execute_path,
+					  _In_ const wchar_t* arguments);
+
+private:
+	bool get_folder_info(_In_ const wchar_t* folder_name,
+						 _Out_ ITaskFolder** folder);
+
+	bool get_task_register_info(_In_ const wchar_t* task_name,
+								_Out_ IRegisteredTask** registered_task,
+								_In_opt_ const wchar_t* folder_name = _default_sched_folder_name);
+
 private:
 	bool _initialized;
-	std::wstring _task_name;
-
 	ITaskService*		_svc;
-	ITaskFolder*		_folder;
-	ITaskDefinition*	_definition;
-	IRegistrationInfo*	_registration_info;
-	ITriggerCollection*	_trigger_collection;
-	ITrigger*			_trigger;
-	IDailyTrigger*		_daily_trigger;
-	IRepetitionPattern* _repetition;
-	IActionCollection*	_action_collection;
-	IExecAction*		_exec_action;
-	IAction*			_action;
 } *PSchedClient;
